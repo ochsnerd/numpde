a13319ea8c3162111747f073de26792f31ed29fa
David Ochsner <ochsnerd@student.ethz.ch>
2019-10-18 20:51:49 +0200

    Absolute path to config file

--- series1_handout/fvm_scalar_1d/src/ancse/config.cpp
+++ series1_handout/fvm_scalar_1d/src/ancse/config.cpp
@@ -6,7 +6,7 @@ nlohmann::json get_global_config() {
         // The path is relative to the binary. This assumes you're building in
         // in a subfolder and therefore, the config file is located in the
         // parent.
-        std::ifstream file("../config.json");
+        std::ifstream file("/home/david/hs19/numpde/exercises/series1_handout/fvm_scalar_1d/config.json");
         assert(file.good());
 
         nlohmann::json config;
