9f105e9b2a993ebbc2f251114249651053d7e1e4
David Ochsner <ochsnerd@student.ethz.ch>
2019-10-28 11:08:10 +0100

    Implement cfl condition

--- series1_handout/fvm_scalar_1d/include/ancse/cfl_condition.hpp
+++ series1_handout/fvm_scalar_1d/include/ancse/cfl_condition.hpp
@@ -19,6 +19,28 @@ class CFLCondition {
     virtual double operator()(const Eigen::VectorXd &u) const = 0;
 };
 
+class ConcreteCFL : public CFLCondition {
+public:
+  ConcreteCFL(const Grid& grid, const Model& model, double cfl_number) :
+    model_{model},
+    dx_{grid.dx},
+    cfl_number_{cfl_number} {};
+
+  virtual double operator()(const Eigen::VectorXd& u) const override {
+    auto abs_compare = [](double a, double b) {return std::abs(a) < std::abs(b);};
+    double max_abs_flux = std::abs(*std::max_element(&(u[0]),
+                                                     &(u[0]) + u.size(),
+                                                     abs_compare));
+
+    return cfl_number_ * dx_ / max_abs_flux;
+  }
+
+private:
+  Model model_;
+  double dx_;
+  double cfl_number_;
+};
+
 
 std::shared_ptr<CFLCondition> make_cfl_condition(const Grid &grid, const Model &model, double cfl_number);
 
--- series1_handout/fvm_scalar_1d/tests/test_cfl_condition.cpp
+++ series1_handout/fvm_scalar_1d/tests/test_cfl_condition.cpp
@@ -2,3 +2,15 @@
 #include <ancse/cfl_condition.hpp>
 #include <gtest/gtest.h>
 
+TEST(TestCFL, Example) {
+  auto cfl1 = ConcreteCFL(make_dummy_grid(),
+                          make_dummy_model(),
+                          1);
+  auto cfl2 = ConcreteCFL(make_dummy_grid(),
+                          make_dummy_model(),
+                          2);
+
+  ASSERT_DOUBLE_EQ(cfl1(Eigen::Vector2d{0.01, 0}), 1);
+  ASSERT_DOUBLE_EQ(cfl1(Eigen::Vector2d{-0.01, 0}), 1);
+  ASSERT_DOUBLE_EQ(cfl2(Eigen::Vector2d{0.01, 0}), 2);
+}
