5e6559d548322ef28b10ade1ac970350b420efed
David Ochsner <ochsnerd@student.ethz.ch>
2019-11-23 17:02:00 +0100

    Implement cfl condition

--- series2_handout/hyp_sys_1d/include/ancse/cfl_condition.hpp
+++ series2_handout/hyp_sys_1d/include/ancse/cfl_condition.hpp
@@ -22,8 +22,41 @@ class CFLCondition {
 };
 
 
+class ConcreteCFLCondition : public CFLCondition {
+public:
+  // Actually already the interface should use this, but then we have
+  // a unwanted dependency (which we already have now - CFLCondition is
+  // forced to use the same Matrix type as Model)
+  using Matrix = Model::Matrix;
+
+  ConcreteCFLCondition(const Grid& grid,
+                       const std::shared_ptr<Model>& model,
+                       double cfl_number)
+    : grid_{grid},
+      model_{model},
+      cfl_number_{cfl_number}
+  {}
+
+  virtual double operator()(const Matrix& u) const override {
+    auto n_cells = grid_.n_cells;
+    auto n_ghost = grid_.n_ghost;
+
+    double a_max = 0.0;
+    for (int i = grid_.n_ghost; i < n_cells - n_ghost; ++i) {
+      a_max = std::max(a_max, model_->max_eigenvalue(u.col(i)));
+    }
+
+    return cfl_number_ * grid_.dx / a_max;
+  }
+
+private:
+  Grid grid_;
+  std::shared_ptr<Model> model_;
+  double cfl_number_;
+};
+
 /// make CFL condition for FVM
-std::shared_ptr<CFLCondition>
+ std::shared_ptr<CFLCondition>
 make_cfl_condition(const Grid &grid,
                    const std::shared_ptr<Model> &model,
                    double cfl_number);
--- series2_handout/hyp_sys_1d/tests/test_cfl_condition.cpp
+++ series2_handout/hyp_sys_1d/tests/test_cfl_condition.cpp
@@ -2,3 +2,37 @@
 #include <ancse/cfl_condition.hpp>
 #include <gtest/gtest.h>
 
+
+void check_cfl_condition_scalar(const CFLCondition &cfl_condition,
+                                 const Grid &grid,
+                                 double cfl_number) {
+  int n_ghost = grid.n_ghost;
+  int n_cells = grid.n_cells;
+
+  Eigen::MatrixXd u(1, n_cells);
+  for (int i = 0; i < n_cells; ++i) {
+    u(0,i) = -i * i;
+  }
+  double max_abs_u = (n_cells - n_ghost - 1) * (n_cells - n_ghost - 1);
+
+  for (int i = 0; i < n_ghost; ++i) {
+    u(0,i) = 2.0 * max_abs_u;
+    u(0, n_cells - n_ghost + i) = 2.0 * max_abs_u;
+  }
+
+  double dt_cfl_approx = cfl_condition(u);
+  double dt_cfl_exact = cfl_number * grid.dx / max_abs_u;
+
+  ASSERT_DOUBLE_EQ(dt_cfl_approx, dt_cfl_exact);
+}
+
+TEST(CFLCondition, Example) {
+    auto n_ghost = 2;
+    auto n_cells = 10 + 2 * n_ghost;
+    auto grid = Grid({0.9, 1.0}, n_cells, n_ghost);
+    auto model = std::make_shared<Burgers>();
+    double cfl_number = 0.5;
+
+    auto cfl_condition = ConcreteCFLCondition(grid, model, cfl_number);
+    check_cfl_condition_scalar(cfl_condition, grid, cfl_number);
+}
