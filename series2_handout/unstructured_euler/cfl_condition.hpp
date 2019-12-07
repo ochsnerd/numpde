9847df98bf4d380a1ad3e738ef85d88f2764f18d
David Ochsner <ochsnerd@student.ethz.ch>
2019-12-07 15:56:40 +0100

    Implement cfl condition

--- series2_handout/unstructured_euler/cfl_condition.hpp
+++ series2_handout/unstructured_euler/cfl_condition.hpp
@@ -13,7 +13,18 @@ class CFLCondition {
         // compute the cfl condition here.
         // you can use `assert_valid_timestep` to check if
         // the computed value is valid.
-        return 0.001;
+
+      // From (33) in the exercise sheet: max attained when n = normalized(u)
+      double max_speed = 0;
+      for (int i = 0; i < U.cols(); ++i) {
+        max_speed = std::max(euler::maxEigenValue(U.col(i)), max_speed);
+      }
+
+      double dt = cfl_number * dx / max_speed;
+
+      assert_valid_timestep(dt);
+
+      return dt;
     }
 
     void assert_valid_timestep(double dt_cfl) const {
