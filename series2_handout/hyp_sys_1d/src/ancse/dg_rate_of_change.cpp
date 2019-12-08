c10fbb640c129cd6f6f27c53a028c3aa413f59b6
David Ochsner <ochsnerd@student.ethz.ch>
2019-12-08 13:16:28 +0100

    Partial implementation of dg_rate_of_change
    Still missing volume integral and code correctness

--- series2_handout/hyp_sys_1d/src/ancse/dg_rate_of_change.cpp
+++ series2_handout/hyp_sys_1d/src/ancse/dg_rate_of_change.cpp
@@ -14,7 +14,31 @@ void DGRateOfChange<NumericalFlux>
 :: eval_numerical_flux (Eigen::MatrixXd &dudt,
                         const Eigen::MatrixXd &u0) const
 {
-    // implement the loop for DG numerical flux term.
+  int n_cells = grid.n_cells;
+  int n_ghost = grid.n_ghost;
+  int n_vars = model->get_nvars();
+
+  double dx = grid.dx;
+  Eigen::VectorXd fL(n_vars), fR(n_vars);
+
+  fR *= 0;
+
+  // quick sanity check
+  assert(dudt.rows() == u0.rows());
+  assert(dudt.cols() == u0.cols());
+  assert(dudt.cols() == n_cells);
+  assert(dudt.rows() == n_vars);
+
+  for (int i = n_ghost - 1; i < n_cells - n_ghost; ++i) {
+    fL = fR;
+
+    // I know this is less than ideal
+    auto uL = dg_handler.build_sol(u0.col(i), cell_center(grid, i) - 0.49 * grid.dx);
+    auto uR = dg_handler.build_sol(u0.col(i), cell_center(grid, i) + 0.49 * grid.dx);
+    fR = numerical_flux(uL, uR);
+
+    dudt.col(i) = (fL - fR) / dx;
+  }
 }
 
 /// DG volume integral term
@@ -43,6 +67,7 @@ std::shared_ptr<RateOfChange> make_dg_rate_of_change(
     const std::shared_ptr<SimulationTime> &simulation_time)
 {
     // Register the other numerical fluxes.
+  REGISTER_NUMERICAL_FLUX("Rusanov_flux", RusanovFlux, RusanovFlux(model))
 
     throw std::runtime_error(
         fmt::format("Unknown numerical flux. {}",
